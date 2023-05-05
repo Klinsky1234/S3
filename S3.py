13ac673e85327f8bd47aba14a62ff5da1b7c26ec
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-05-05 21:57:45 +0300

    HW commit

--- S3.py
+++ S3.py
@@ -72,4 +72,54 @@ def S3_task_1_1():
         print("одинаково")
 
 
-S3_task_1_1()
+# S3_task_1_1()
+
+# Задача 2. Напишите программу, которая позволит пользователю
+# заполнить анкету, последовательно вводя в программу:
+# - имя;
+# - возраст;
+# - хобби;
+# - любимое животное.
+# После окончания ввода, выводится заполненная анкета.
+
+
+def task_2():
+    slovar = {}
+    slovar["name"] = input("Введите ваше имя: ")
+    slovar["age"] = input("Введите ваш возраст: ")
+    slovar["hobby"] = input("Введите ваше хобби: ")
+    slovar["pet"] = input("Введите ваше любимое животное: ")
+    print(slovar)
+
+
+# task_2()
+
+
+def task_2_1():
+    form = dict(
+        имя=input("Введите ваше имя: "),
+        возраст=input("Введите ваш возраст: "),
+        хобби=input("Введите ваше хобби: "),
+        Животное=input("Введите ваше любимое животное: "),
+    )
+    for key, value in form.items():
+        print("{0}:{1}".format(key, value))
+
+    for k, v in form.items():
+        print(k + ":", v)
+
+
+# task_2_1()
+
+# Задача 3. Напишите скрипт генератора паролей заданной длины, состоящих из латинских букв и чисел.
+import random
+
+
+def task_3():
+    x = input("Enter len password: ")
+    length = "qwertyuiop123asdfghjjkl!*45657zxcv(_bnm,./7890"
+    password = [random.randint(0, 10) for el in range(len)]
+    print(password)
+
+
+# task_3()
